8d46dd1127db8c58af7fb557f7571e2e25cf7e56
Hancel <hancel@gmail.com>
2021-10-21 23:26:38 -0500

    Updates

---
+++ precache-manifest.7e73a998b0dbc1515f9b734d9b0fbbcf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "579942211b23930aa006c1b18d74214d",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "219def320ba3da4750bb",
+    "url": "/Portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "1ec047a434018f63074a",
+    "url": "/Portfolio/static/css/main.8906e570.chunk.css"
+  },
+  {
+    "revision": "219def320ba3da4750bb",
+    "url": "/Portfolio/static/js/2.1e6864e8.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/Portfolio/static/js/2.1e6864e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ec047a434018f63074a",
+    "url": "/Portfolio/static/js/main.9f17cb1f.chunk.js"
+  },
+  {
+    "revision": "c125469937b460e6de34",
+    "url": "/Portfolio/static/js/runtime-main.3379861c.js"
+  }
+]);
